92e39d8d59f466157ae0ee3a7e84af1f7c436473
Salma <salma99987.yasser@gmail.com>
2025-11-01 18:22:22 +0200

    Added initial insert

--- appointment.cpp
+++ appointment.cpp
@@ -1,17 +1,27 @@
 #include <cstring>
+#include<fstream>
 using namespace std;
 class appointment{
+public:
     char id[15];
     char date[30];
     char docId[15];
-   public:
-    void add(char id[15],char date[30],char docId[15]){
-        strcpy(this->id,id);
-        strcpy(this->date,date);
-        strcpy(this->date,date);
+    const static int size = 1000;
 
+    void addAppointment(fstream &file , appointment &a){
+        char record[size];
+        strcpy(record,a.id);
+        strcat(record,"|");
+        strcat(record,a.date);
+        strcat(record,"|");
+        strcat(record,a.docId);
+        strcat(record,"\n");
+        short length = strlen(record);
+        file << length;
+        file.write(record,length);
     }
+
     void del(){};
     void update(){};
-    
+
 };
